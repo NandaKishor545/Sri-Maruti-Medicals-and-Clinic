82a1e756149bd83217ef7d957ded1305d129ea23
NandaKishor545 <shilpascreativity785@gmail.com>
2025-04-17 12:04:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Sri Maruti Medicals & Clinic",
+  botOwner: 'Sri Maruti Medicals & Clinic', // Your name
+  ownerNumber: '919945256642', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
